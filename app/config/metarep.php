a22bcb73effcb830b95e10ebe30ebd9f728fa422
Johannes Goll <jgoll@jcvi.org>
2010-06-29 16:22:20 -0400

    added METAREP configuration file

--- app/config/metarep.php
+++ app/config/metarep.php
@@ -11,64 +11,64 @@
  * METAREP Web Root
  */
 
-define('METAREP_WEB_ROOT','/opt/www/metarep/htdocs/metarep');
+define('METAREP_WEB_ROOT',' /<your-installation-dir>/apache-2.2.14/htodocs/metarep');
 
 /**
  * Directory to store tmporary files
  */
 
-define('METAREP_TMP_DIR','/opt/www/metarep/tmp');
+define('METAREP_TMP_DIR','/tmp');
 
 /**
  * Solr instance dir; contains Solr configuration files
  */
 
-define('SOLR_INSTANCE_DIR','/opt/software/apache-solr/solr');
+define('SOLR_INSTANCE_DIR','/<your-installation-dir>/apache-solr-1.4.0/metarep-solr');
 
 /**
  * Solr data dir; contains index files
  */
 
-define('SOLR_DATA_DIR','/solr-index');
+define('SOLR_DATA_DIR','/<your-installation-dir>/apache-solr-1.4.0/metarep-solr/data');
 
 /**
  * Solr port
  */
 
-define('SOLR_PORT','8989');
+define('SOLR_PORT','1234');
 
 /**
  * Solr master server url
  */
 
-define('SOLR_MASTER_HOST','172.20.13.24');
+define('SOLR_MASTER_HOST','localhost');
 
 /**
  * Solr slave server url; define if you use a 
  * load balanced master/slave replication set up 
  */
 
-define('SOLR_SLAVE_HOST','172.20.13.25');
+//define('SOLR_SLAVE_HOST','172.20.13.25');
 
 /**
  * Solr big ip; define if you use a 
  * load balanced master/slave replication set up ;
  */
 
-define('SOLR_BIG_IP_HOST','172.20.12.25:8989');
+//define('SOLR_BIG_IP_HOST','172.20.12.25:8989');
 
 /**
  * Email to send bug reports and feature requests
  */
 
-define('METAREP_SUPPORT_EMAIL','metarep-support@jcvi.org');
+//define('METAREP_SUPPORT_EMAIL','metarep-support@jcvi.org');
 
 /**
  * Email extension that is used to identify internal users
  * Internal users can access to all datasets
  */
 
-define('INTERNAL_EMAIL_EXTENSION','jcvi.org');
+//define('INTERNAL_EMAIL_EXTENSION','jcvi.org');
 
 /**
  * Top number of hits shown for annotation data types
